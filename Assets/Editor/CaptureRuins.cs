08236be5fb480d5d0a6aacae55be3ef90bdfee5c
yun.wang <wy19910222@hotmail.com>
2023-10-07 01:16:09 +0800

    6级田截图

---
+++ Assets/Editor/CaptureRuins.cs
@@ -0,0 +1,160 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2023-10-06 12:16:55 107
+ * @LastEditor: wangyun
+ * @EditTime: 2023-10-06 12:16:55 112
+ */
+
+using System.IO;
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class CaptureRuinsConfig : EditorWindow {
+	[MenuItem("Window/CaptureRuins")]
+	private static void Open() {
+		GetWindow<CaptureRuinsConfig>("王者遗迹截图").Show();
+	}
+	
+	private void OnGUI() {
+		if (CaptureRuins.IsRunning) {
+			if (GUILayout.Button("取消截图")) {
+				EditorApplication.ExecuteMenuItem("Assets/StopCaptureRuins");
+			}
+		} else {
+			if (GUILayout.Button("开始截图")) {
+				EditorApplication.ExecuteMenuItem("Assets/StartCaptureRuins");
+			}
+		}
+	}
+}
+
+public static class CaptureRuins {
+	private static EditorCoroutine s_CO;
+	public static bool IsRunning => s_CO != null;
+	
+	[MenuItem("Assets/StartCaptureRuins", priority = -1)]
+	private static void Enable() {
+		Disable();
+		s_CO = EditorCoroutineManager.StartCoroutine(IECapture());
+		Debug.Log("开始进行王者遗迹截图");
+	}
+
+	[MenuItem("Assets/StopCaptureRuins", priority = -1)]
+	private static void Disable() {
+		if (s_CO != null) {
+			EditorCoroutineManager.StopCoroutine(s_CO);
+			s_CO = null;
+			Debug.Log("取消王者遗迹截图");
+		}
+	}
+
+	private static IEnumerator IECapture() {
+		{
+			// 北边遗迹截图
+			var ie = IEDoCapture(new Vector2Int(1064, 492), new Vector2Int(1060, 820), new Vector2Int(1100, 600), "NorthRuins");
+			while (ie.MoveNext()) {
+				yield return ie.Current;
+			}
+		}
+		{
+			// 南边遗迹截图
+			var ie = IEDoCapture(new Vector2Int(856, 624), new Vector2Int(1030, 460), new Vector2Int(1100, 460), "SouthRuins");
+			while (ie.MoveNext()) {
+				yield return ie.Current;
+			}
+		}
+		s_CO = null;
+	}
+
+	private static IEnumerator IEDoCapture(Vector2Int ruinsIconPos, Vector2Int gotoBtnPos, Vector2Int offset, string filename) {
+		if (Recognize.CurrentScene != Recognize.Scene.OUTSIDE) {
+			yield break;
+		}
+		{
+			// 先拉远，保证跳转遗迹后会重置视距
+			var ie = Operation.Zoom(-80);
+			while (ie.MoveNext()) {
+				yield return ie.Current;
+			}
+		}
+		yield return new EditorWaitForSeconds(0.2F);
+		Operation.Click(155, 160);	// 左上角地图按钮
+		yield return new EditorWaitForSeconds(0.2F);
+		Operation.Click(ruinsIconPos.x, ruinsIconPos.y);	// 选择北方遗迹
+		yield return new EditorWaitForSeconds(0.2F);
+		Operation.Click(gotoBtnPos.x, gotoBtnPos.y);	// 前往按钮
+		yield return new EditorWaitForSeconds(0.2F);
+		{
+			// 再拉远，保证6级田能显示但只显示成图标
+			var ie = Operation.Zoom(-19);
+			while (ie.MoveNext()) {
+				yield return ie.Current;
+			}
+		}
+		yield return new EditorWaitForSeconds(0.2F);
+		for (int i = 0; i < 2; i++) {
+			// 往左上拖动
+			var ie = Operation.NoInertiaDrag(960 - offset.x / 2, 540 - offset.y / 2, 960 + offset.x / 2, 540 + offset.y / 2, 0.5F);
+			while (ie.MoveNext()) {
+				yield return ie.Current;
+			}
+		}
+		yield return new EditorWaitForSeconds(0.5F);
+		const int screenWidth = 1920, screenHeight = 1080;
+		const int column = 4, row = 5;
+		const int width = 1500, height = 540;
+		Color32[,][,] colorsBlocks = new Color32[column,row][,];
+		for (int i = 0; i < row; ++i) {
+			for (int j = 0; j < column; ++j) {
+				colorsBlocks[j, i] = ScreenshotUtils.GetColorsOnScreen((screenWidth - width) / 2, (screenHeight - height) / 2 - 20, width, height);
+				if (j < column - 1) {
+					// 往右拖动
+					var ie = Operation.NoInertiaDrag((screenWidth + width) / 2, 540, (screenWidth - width) / 2, 540, 1F);
+					while (ie.MoveNext()) {
+						yield return ie.Current;
+					}
+					yield return new EditorWaitForSeconds(0.5F);
+				}
+			}
+			if (i < row - 1) {
+				for (int j = 0; j < column; ++j) {
+					if (j < column - 1) {
+						// 往左拖动
+						var ie = Operation.NoInertiaDrag((screenWidth - width) / 2, 540, (screenWidth + width) / 2, 540, 0.5F);
+						while (ie.MoveNext()) {
+							yield return ie.Current;
+						}
+					}
+				}
+				{
+					// 往下拖动
+					var ie = Operation.NoInertiaDrag(960, (screenHeight + height) / 2, 960, (screenHeight - height) / 2, 1F);
+					while (ie.MoveNext()) {
+						yield return ie.Current;
+					}
+					yield return new EditorWaitForSeconds(0.5F);
+				}
+			}
+		}
+		Color32[] colors = new Color32[width * height * column * row];
+		for (int i = row - 1; i >= 0; --i) {
+			for (int j = 0; j < column; ++j) {
+				for (int y = height - 1; y >= 0; --y) {
+					for (int x = 0; x < width; ++x) {
+						int totalX = j * width + x;
+						int totalY = (row - 1 - i) * height + (height - 1 - y);
+						Color32 c = colorsBlocks[j, i][x, y];
+						colors[totalY * column * width + totalX] = c;
+					}
+				}
+			}
+		}
+		Texture2D tex = new Texture2D(column * width, row * height);
+		tex.SetPixels32(colors);
+		tex.Apply();
+		byte[] bytes = tex.EncodeToPNG();
+		File.WriteAllBytes($"Assets/{filename}.png", bytes);
+		AssetDatabase.Refresh();
+	}
+}
--- Assets/Editor/Operation.cs
+++ Assets/Editor/Operation.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 
 public static class Operation {
@@ -43,6 +44,41 @@ public static class Operation {
 		MouseUtils.SetMousePos(x2, y2);
 		MouseUtils.LeftUp();
 		
+		MouseUtils.SetMousePos(oldPos.x, oldPos.y);
+	}
+
+	public static IEnumerator NoInertiaDrag(int x1, int y1, int x2, int y2, float duration = 0.2F) {
+		Vector2Int oldPos = MouseUtils.GetMousePos();
+		
+		MouseUtils.SetMousePos(x1, y1);
+		MouseUtils.LeftDown();
+		float duration1 = Mathf.Max(duration - 0.1F, 0.1F);
+		long startTime = DateTime.Now.Ticks;
+		while (true) {
+			yield return null;
+			float percent = (DateTime.Now.Ticks - startTime) / (duration1 * 10000000);
+			if (percent >= 1) {
+				break;
+			}
+			percent = 1 - Mathf.Pow(1 - percent, 2);
+			MouseUtils.SetMousePos(Mathf.RoundToInt(Mathf.Lerp(x1, x2, percent)), Mathf.RoundToInt(Mathf.Lerp(y1, y2, percent)));	// 加入按钮
+		}
+		MouseUtils.SetMousePos(x2, y2);
+		yield return new EditorWaitForSeconds(0.1F);
+		MouseUtils.LeftUp();
+		
+		MouseUtils.SetMousePos(oldPos.x, oldPos.y);
+	}
+
+	public static IEnumerator Zoom(int value) {
+		Vector2Int oldPos = MouseUtils.GetMousePos();
+		MouseUtils.SetMousePos(960, 540);	// 鼠标移动到屏幕中央
+		int absValue = Mathf.Abs(value);
+		int direction = value / absValue;
+		for (int i = 0; i < absValue; ++i) {
+			MouseUtils.ScrollWheel(direction);
+			yield return new EditorWaitForSeconds(0.02F);
+		}
 		MouseUtils.SetMousePos(oldPos.x, oldPos.y);
 	}
 }
