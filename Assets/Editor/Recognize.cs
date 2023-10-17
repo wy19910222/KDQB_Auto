61ff7e6d218ec75a6ce789938ca1af7698b591f0
yun.wang <wy19910222@hotmail.com>
2023-10-18 06:10:00 +0800

    修复比较目标是纯黑色时永远返回false的bug；新增矩形比较；

--- Assets/Editor/Recognize.cs
+++ Assets/Editor/Recognize.cs
@@ -144,17 +144,38 @@ public static partial class Recognize {
 	};
 
 #region Base
+	public static float ApproximatelyRect(Color32[,] realColors, Color32[,] targetColors, float thresholdMulti = 1) {
+		int realWidth = realColors.GetLength(0);
+		int targetWidth = targetColors.GetLength(0);
+		if (realWidth != targetWidth) {
+			return 0;
+		}
+		int realHeight = realColors.GetLength(1);
+		int targetHeight = targetColors.GetLength(1);
+		if (realHeight != targetHeight) {
+			return 0;
+		}
+		int approximatelyCount = 0;
+		for (int y = 0; y < realHeight; ++y) {
+			for (int x = 0; x < realWidth; ++x) {
+				if (Approximately(realColors[x, y], targetColors[x, y], thresholdMulti)) {
+					++approximatelyCount;
+				}
+			}
+		}
+		return (float) approximatelyCount / (realWidth * realHeight);
+	}
+	
 	public static bool Approximately(Color32 realColor, Color32 targetColor, float thresholdMulti = 1) {
 		float targetR = targetColor.r;
 		float targetG = targetColor.g;
 		float targetB = targetColor.b;
-		float deltaR = Mathf.Abs(realColor.r / targetR - 1);
-		float deltaG = Mathf.Abs(realColor.g / targetG - 1);
-		float deltaB = Mathf.Abs(realColor.b / targetB - 1);
-		// Debug.LogError($"{deltaR}, {deltaG}, {deltaB}");
-		return deltaR < GetThreshold(targetR) * thresholdMulti &&
-				deltaG < GetThreshold(targetG) * thresholdMulti &&
-				deltaB < GetThreshold(targetB) * thresholdMulti;
+		float deltaR = targetR == 0 ? realColor.r - 1 : Mathf.Abs(realColor.r / targetR - 1);
+		float deltaG = targetG == 0 ? realColor.g - 1 : Mathf.Abs(realColor.g / targetG - 1);
+		float deltaB = targetB == 0 ? realColor.b - 1 : Mathf.Abs(realColor.b / targetB - 1);
+		return deltaR <= GetThreshold(targetR) * thresholdMulti &&
+				deltaG <= GetThreshold(targetG) * thresholdMulti &&
+				deltaB <= GetThreshold(targetB) * thresholdMulti;
 	}
 	// {0.4F, 0.65F}: 出征界面有个特殊弹窗很浅
 	private static readonly Dictionary<float, float> COVER_COEFFICIENT_DICT = new() {
@@ -169,13 +190,13 @@ public static partial class Recognize {
 			float targetG = targetColor.g * coefficient;
 			float targetB = targetColor.b * coefficient;
 			// 当目标值为0，则1以内算相近，否则按比例
-			float deltaR = targetR == 0 ? realColor.r <= 1 ? 0 : 1 : Mathf.Abs(realColor.r / targetR - 1);
-			float deltaG = targetG == 0 ? realColor.g <= 1 ? 0 : 1 : Mathf.Abs(realColor.g / targetG - 1);
-			float deltaB = targetB == 0 ? realColor.b <= 1 ? 0 : 1 : Mathf.Abs(realColor.b / targetB - 1);
+			float deltaR = targetR == 0 ? realColor.r - 1 : Mathf.Abs(realColor.r / targetR - 1);
+			float deltaG = targetG == 0 ? realColor.g - 1 : Mathf.Abs(realColor.g / targetG - 1);
+			float deltaB = targetB == 0 ? realColor.b - 1 : Mathf.Abs(realColor.b / targetB - 1);
 			// Debug.LogError($"第{coverCount}层：{deltaR}, {deltaG}, {deltaB}");
-			if (deltaR < GetThreshold(targetR) * thresholdMulti &&
-					deltaG < GetThreshold(targetG) * thresholdMulti &&
-					deltaB < GetThreshold(targetB) * thresholdMulti) {
+			if (deltaR <= GetThreshold(targetR) * thresholdMulti &&
+					deltaG <= GetThreshold(targetG) * thresholdMulti &&
+					deltaB <= GetThreshold(targetB) * thresholdMulti) {
 				return coverCount;
 			}
 		}
