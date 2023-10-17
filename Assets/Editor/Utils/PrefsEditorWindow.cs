6cd65fcbd9c025b15ca1ca54c0f227fcae29fcd2
yun.wang <wy19910222@hotmail.com>
2023-10-18 06:07:42 +0800

    其他格式都转成json字符串储存；窗口新增下拉菜单；新增调试模式；

--- Assets/Editor/Utils/PrefsEditorWindow.cs
+++ Assets/Editor/Utils/PrefsEditorWindow.cs
@@ -7,10 +7,36 @@
 
 using System;
 using System.Reflection;
+using Newtonsoft.Json;
+using UnityEngine;
 using UnityEditor;
 
 public class PrefsEditorWindow<T> : EditorWindow {
+	protected bool m_Debug;
+	
 	protected virtual void OnEnable() {
+		LoadOptions();
+	}
+
+	protected virtual void OnDisable() {
+		SaveOptions();
+	}
+
+	protected virtual void OnMenu(GenericMenu menu) {
+		menu.AddItem(new GUIContent("调试模式"), m_Debug, () => m_Debug = !m_Debug);
+		menu.AddItem(new GUIContent("重置选项"), false, LoadOptions);
+		menu.AddItem(new GUIContent("保存选项"), false, SaveOptions);
+	}
+
+	private void ShowButton(Rect rect) {
+		if (GUI.Button(rect, new GUIContent("", "Show menu"), EditorStyles.toolbarDropDown)) {
+			GenericMenu menu = new GenericMenu();
+			OnMenu(menu);
+			menu.ShowAsContext();
+		}
+	}
+
+	private static void LoadOptions() {
 		Type type = typeof(T);
 		FieldInfo[] fis = type.GetFields(BindingFlags.Static | BindingFlags.Public);
 		foreach (FieldInfo fi in fis) {
@@ -24,11 +50,16 @@ public class PrefsEditorWindow<T> : EditorWindow {
 				fi.SetValue(null, EditorPrefs.GetFloat(key, (float) defaultValue));
 			} else if (fi.FieldType == typeof(string)) {
 				fi.SetValue(null, EditorPrefs.GetString(key, (string) defaultValue));
+			} else {
+				string defaultValueStr = JsonConvert.SerializeObject(defaultValue);
+				string valueStr = EditorPrefs.GetString(key, defaultValueStr);
+				object value = JsonConvert.DeserializeObject(valueStr, fi.FieldType);
+				fi.SetValue(null, value);
 			}
 		}
 	}
 
-	protected virtual void OnDisable() {
+	private static void SaveOptions() {
 		Type type = typeof(T);
 		FieldInfo[] fis = type.GetFields(BindingFlags.Static | BindingFlags.Public);
 		foreach (FieldInfo fi in fis) {
@@ -42,6 +73,9 @@ public class PrefsEditorWindow<T> : EditorWindow {
 				EditorPrefs.SetFloat(key, (float) value);
 			} else if (fi.FieldType == typeof(string)) {
 				EditorPrefs.SetString(key, (string) value);
+			} else {
+				string valueStr = JsonConvert.SerializeObject(value);
+				EditorPrefs.SetString(key, valueStr);
 			}
 		}
 	}
