ac9e7e102f60da3a74e69a884a5f7afc865a6ca5
yun.wang <wy19910222@hotmail.com>
2023-10-18 01:14:38 +0800

    深海寻宝脚本改版

--- Assets/Editor/DeepSea.cs
+++ Assets/Editor/DeepSea.cs
@@ -5,6 +5,7 @@
  * @EditTime: 2023-10-12 00:00:51 756
  */
 
+using System;
 using System.Collections;
 using UnityEditor;
 using UnityEngine;
@@ -20,7 +21,22 @@ public class DeepSeaConfig : PrefsEditorWindow<DeepSea> {
 	private void OnGUI() {
 		DeepSea.ACTIVITY_ORDER = EditorGUILayout.IntSlider("活动排序（活动排在第几个）", DeepSea.ACTIVITY_ORDER, 1, 20);
 		DeepSea.DETECTOR_COUNT = EditorGUILayout.IntSlider("拥有探测器", DeepSea.DETECTOR_COUNT, 1, 3);
-		DeepSea.CLICK_INTERVAL = Mathf.Clamp(EditorGUILayout.IntField("点击间隔时间（秒）", DeepSea.CLICK_INTERVAL), 60, 1200);
+		// DeepSea.CLICK_INTERVAL = Mathf.Clamp(EditorGUILayout.IntField("点击间隔时间（秒）", DeepSea.CLICK_INTERVAL), 60, 1200);
+		EditorGUILayout.BeginHorizontal();
+		EditorGUI.BeginChangeCheck();
+		TimeSpan ts = DeepSea.Countdown;
+		int hours = EditorGUILayout.IntField("倒计时", ts.Hours);
+		float prevLabelWidth = EditorGUIUtility.labelWidth;
+		EditorGUIUtility.labelWidth = 8F;
+		int minutes = EditorGUILayout.IntField(":", ts.Minutes);
+		int seconds = EditorGUILayout.IntField(":", ts.Seconds);
+		EditorGUIUtility.labelWidth = prevLabelWidth;
+		if (EditorGUI.EndChangeCheck()) {
+			DeepSea.Countdown = new TimeSpan(hours, minutes, seconds);
+		}
+		EditorGUILayout.EndHorizontal();
+		DeepSea.TRY_COUNT = EditorGUILayout.IntSlider("尝试次数", DeepSea.TRY_COUNT, 1, 4);
+		
 		GUILayout.Space(5F);
 		if (DeepSea.IsRunning) {
 			if (GUILayout.Button("关闭")) {
@@ -32,13 +48,37 @@ public class DeepSeaConfig : PrefsEditorWindow<DeepSea> {
 			}
 		}
 	}
+	
+	private void OnEnable() {
+		base.OnEnable();
+		EditorApplication.update += Repaint;
+		string key = $"{nameof(DeepSea)}.{nameof(DeepSea.TargetDT)}";
+		string defaultValueStr = (DateTime.Now + DeepSea.DEFAULT_COUNTDOWN).Ticks + "";
+		string targetDTStr = EditorPrefs.GetString(key, defaultValueStr);
+		DeepSea.TargetDT = new DateTime(Convert.ToInt64(targetDTStr));
+	}
+
+	private void OnDisable() {
+		base.OnDisable();
+		EditorApplication.update -= Repaint;
+		string key = $"{nameof(DeepSea)}.{nameof(DeepSea.TargetDT)}";
+		string targetDTStr = DeepSea.TargetDT.Ticks + "";
+		EditorPrefs.SetString(key, targetDTStr);
+	}
 }
 
 public class DeepSea {
 	public static int ACTIVITY_ORDER = 8;	// 活动排序
 	public static int DETECTOR_COUNT = 3;	// 拥有探测器数量
-	public static int CLICK_INTERVAL = 300;	// 点击间隔
-	
+	public static TimeSpan DEFAULT_COUNTDOWN = new TimeSpan(8, 0, 0);	// 倒计时
+	public static int TRY_COUNT = 3;	// 倒计时结束时尝试3次
+	// public static int CLICK_INTERVAL = 120;	// 点击间隔
+
+	public static DateTime TargetDT { get; set; }
+	public static TimeSpan Countdown {
+		get => TargetDT - DateTime.Now;
+		set => TargetDT = DateTime.Now + value;
+	}
 	private static EditorCoroutine s_CO;
 	public static bool IsRunning => s_CO != null;
 
@@ -61,58 +101,62 @@ public class DeepSea {
 	private static IEnumerator Update() {
 		while (true) {
 			yield return null;
+			if (DateTime.Now < TargetDT) {
+				continue;
+			}
 			// 有窗口打开着
 			if (Recognize.IsWindowCovered) {
 				continue;
 			}
-			// 如果是世界界面远景，则没有显示活动按钮，需要先切换到近景
-			if (Recognize.CurrentScene == Recognize.Scene.OUTSIDE && Recognize.IsOutsideFaraway) {
-				while (Recognize.IsOutsideFaraway) {
-					Vector2Int oldPos = MouseUtils.GetMousePos();
-					MouseUtils.SetMousePos(960, 540);	// 鼠标移动到屏幕中央
-					MouseUtils.ScrollWheel(1);
-					MouseUtils.SetMousePos(oldPos.x, oldPos.y);
+
+			for (int tryCount = 0; tryCount < TRY_COUNT; ++tryCount) {
+				// 如果是世界界面远景，则没有显示活动按钮，需要先切换到近景
+				if (Recognize.CurrentScene == Recognize.Scene.OUTSIDE && Recognize.IsOutsideFaraway) {
+					while (Recognize.IsOutsideFaraway) {
+						Vector2Int oldPos = MouseUtils.GetMousePos();
+						MouseUtils.SetMousePos(960, 540);	// 鼠标移动到屏幕中央
+						MouseUtils.ScrollWheel(1);
+						MouseUtils.SetMousePos(oldPos.x, oldPos.y);
+						yield return new EditorWaitForSeconds(0.1F);
+					}
+				}
+				Debug.Log("活动按钮");
+				Operation.Click(1880, 290);	// 活动按钮
+				yield return new EditorWaitForSeconds(0.5F);
+				Debug.Log("拖动以显示活动标签页");
+				int orderOffsetX = (ACTIVITY_ORDER - 4) * 137;
+				while (orderOffsetX > 0) {
+					int dragDistance = 138 * 4;
+					// 往左上拖动
+					var ie = Operation.NoInertiaDrag(1190, 200, 1190 - dragDistance, 200, 0.5F);
+					while (ie.MoveNext()) {
+						yield return ie.Current;
+					}
 					yield return new EditorWaitForSeconds(0.1F);
+					orderOffsetX -= dragDistance;
 				}
-			}
-			Debug.Log("活动按钮");
-			Operation.Click(1880, 290);	// 活动按钮
-			yield return new EditorWaitForSeconds(0.5F);
-			Debug.Log("拖动以显示活动标签页");
-			int orderOffsetX = (ACTIVITY_ORDER - 4) * 137;
-			while (orderOffsetX > 0) {
-				int dragDistance = 138 * 4;
-				// 往左上拖动
-				var ie = Operation.NoInertiaDrag(1190, 200, 1190 - dragDistance, 200, 0.5F);
-				while (ie.MoveNext()) {
-					yield return ie.Current;
-				}
+				Debug.Log("活动标签页");
+				Operation.Click(1190 + orderOffsetX, 200);	// 活动标签页
 				yield return new EditorWaitForSeconds(0.1F);
-				orderOffsetX -= dragDistance;
+				Debug.Log("点击探测器");
+				for (int i = 0; i < DETECTOR_COUNT; ++i) {
+					Debug.Log($"点击探测器{i + 1}");
+					Operation.Click(808 + 153 * i, 870);	// 探测器
+					yield return new EditorWaitForSeconds(0.2F);
+					Operation.Click(808 + 153 * i, 870);	// 探测器
+					yield return new EditorWaitForSeconds(0.2F);
+					Operation.Click(808 + 153 * i, 870);	// 探测器
+					yield return new EditorWaitForSeconds(0.2F);
+					Operation.Click(808 + 153 * i, 870);	// 探测器
+					yield return new EditorWaitForSeconds(0.2F);
+				}
+				while (Recognize.IsWindowCovered) {	// 如果有窗口，多点几次返回按钮
+					Debug.Log("关闭窗口");
+					Operation.Click(735, 128);	// 左上角返回按钮
+					yield return new EditorWaitForSeconds(0.2F);
+				}
 			}
-			Debug.Log("活动标签页");
-			Operation.Click(1190 + orderOffsetX, 200);	// 活动标签页
-			yield return new EditorWaitForSeconds(0.1F);
-			Debug.Log("点击探测器");
-			for (int i = 0; i < DETECTOR_COUNT; ++i) {
-				Debug.Log($"点击探测器{i + 1}");
-				Operation.Click(808 + 153 * i, 870);	// 探测器
-				yield return new EditorWaitForSeconds(0.2F);
-				Operation.Click(808 + 153 * i, 870);	// 探测器
-				yield return new EditorWaitForSeconds(0.2F);
-				Operation.Click(808 + 153 * i, 870);	// 探测器
-				yield return new EditorWaitForSeconds(0.2F);
-				Operation.Click(808 + 153 * i, 870);	// 探测器
-				yield return new EditorWaitForSeconds(0.2F);
-			}
-			while (Recognize.IsWindowCovered) {	// 如果有窗口，多点几次返回按钮
-				Debug.Log("关闭窗口");
-				Operation.Click(735, 128);	// 左上角返回按钮
-				yield return new EditorWaitForSeconds(0.2F);
-			}
-			
-			// 60秒后再重新检查
-			yield return new EditorWaitForSeconds(CLICK_INTERVAL);
+			TargetDT = DateTime.Now + DEFAULT_COUNTDOWN;
 		}
 		// ReSharper disable once IteratorNeverReturns
 	}
