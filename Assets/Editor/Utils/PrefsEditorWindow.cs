692abe5ed099092f280ec29d7caa8d2c93bbfc24
yun.wang <wy19910222@hotmail.com>
2023-10-18 17:56:32 +0800

    新增复杂数据备份功能

--- Assets/Editor/Utils/PrefsEditorWindow.cs
+++ Assets/Editor/Utils/PrefsEditorWindow.cs
@@ -6,6 +6,8 @@
  */
 
 using System;
+using System.IO;
+using System.Text;
 using System.Reflection;
 using Newtonsoft.Json;
 using UnityEngine;
@@ -13,6 +15,7 @@ using UnityEditor;
 
 public class PrefsEditorWindow<T> : EditorWindow {
 	protected bool m_Debug;
+	protected bool m_AutoBackupComplexData = true;
 	
 	protected virtual void OnEnable() {
 		LoadOptions();
@@ -20,12 +23,18 @@ public class PrefsEditorWindow<T> : EditorWindow {
 
 	protected virtual void OnDisable() {
 		SaveOptions();
+		if (m_AutoBackupComplexData) {
+			BackupComplexData();
+		}
 	}
 
 	protected virtual void OnMenu(GenericMenu menu) {
 		menu.AddItem(new GUIContent("调试模式"), m_Debug, () => m_Debug = !m_Debug);
 		menu.AddItem(new GUIContent("重置选项"), false, LoadOptions);
 		menu.AddItem(new GUIContent("保存选项"), false, SaveOptions);
+		menu.AddItem(new GUIContent("自动备份复杂数据"), m_AutoBackupComplexData, () => m_AutoBackupComplexData = !m_AutoBackupComplexData);
+		menu.AddItem(new GUIContent("备份复杂数据"), false, BackupComplexData);
+		menu.AddItem(new GUIContent("恢复复杂数据"), false, RecoverComplexData);
 	}
 
 	private void ShowButton(Rect rect) {
@@ -33,6 +42,57 @@ public class PrefsEditorWindow<T> : EditorWindow {
 			GenericMenu menu = new GenericMenu();
 			OnMenu(menu);
 			menu.ShowAsContext();
+		}
+	}
+
+	private static void BackupComplexData() {
+		// 复杂数据以文件形式再备份一份
+		Type type = typeof(T);
+		FieldInfo[] fis = type.GetFields(BindingFlags.Static | BindingFlags.Public);
+		foreach (FieldInfo fi in fis) {
+			if (!fi.FieldType.IsValueType) {
+				string key = $"{type.Name}.{fi.Name}";
+				string filePath = $"{Application.dataPath}/PersistentData/{key}.txt";
+				FileInfo file = new FileInfo(filePath);
+				if (file.Exists) {
+					file.IsReadOnly = false;
+					file.Delete();
+				} else {
+					DirectoryInfo directory = file.Directory;
+					if (directory is {Exists: false}) {
+						directory.Create();
+					}
+				}
+				using (FileStream fs = file.OpenWrite()) {
+					object value = fi.GetValue(null);
+					string valueStr = JsonConvert.SerializeObject(value);
+					byte[] bytes = Encoding.UTF8.GetBytes(valueStr);
+					fs.Write(bytes, 0, bytes.Length); 
+					fs.Flush();
+				}
+				file.IsReadOnly = true;
+			}
+		}
+	}
+
+	private static void RecoverComplexData() {
+		// 复杂数据丢失时从文件恢复
+		Type type = typeof(T);
+		FieldInfo[] fis = type.GetFields(BindingFlags.Static | BindingFlags.Public);
+		foreach (FieldInfo fi in fis) {
+			if (!fi.FieldType.IsValueType) {
+				string key = $"{type.Name}.{fi.Name}";
+				string filePath = $"{Application.dataPath}/PersistentData/{key}.txt";
+				if (File.Exists(filePath)) {
+					string valueStr = File.ReadAllText(filePath);
+					try {
+						object value = JsonConvert.DeserializeObject(valueStr, fi.FieldType);
+						fi.SetValue(null, value);
+					} catch (Exception e) {
+						Debug.LogError($"恢复文件失败：{e}");
+					}
+				}
+			}
 		}
 	}
 
