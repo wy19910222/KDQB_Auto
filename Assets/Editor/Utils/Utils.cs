1637e04110b15d63623a1dfb5acb70bc7f1525ad
yun.wang <wy19910222@hotmail.com>
2023-11-14 07:34:50 +0800

    工具类

---
+++ Assets/Editor/Utils/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+public static class Utils {
+	public static string GetEnumInspectorName(object enumValue) {
+		Type enumType = enumValue.GetType();
+		string enumName = Enum.GetName(enumType, enumValue);
+		FieldInfo field = enumType.GetField(enumName);
+		object[] attrs = field.GetCustomAttributes(false);
+		foreach (var attr in attrs) {
+			if (attr is InspectorNameAttribute inspectorName) {
+				enumName = inspectorName.displayName;
+				break;
+			}
+		}
+		return enumName;
+	}
+}
