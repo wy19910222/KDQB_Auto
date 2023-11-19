e510f656aa7359c0795e21f2fbc5e45bf98fb0ae
yun.wang <wy19910222@hotmail.com>
2023-11-19 18:55:30 +0800

    新增打印队列头像特征接口，调整判断阈值

--- Assets/Editor/Recognize_Hero.cs
+++ Assets/Editor/Recognize_Hero.cs
@@ -6,7 +6,9 @@
  */
 
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEditor;
 
 public static partial class Recognize {
 	public static bool IsOrangeHeroInGroup() {
@@ -30,11 +32,14 @@ public static partial class Recognize {
 		return false;
 	}
 
+	public static int GetDANGroupNumber() {
+		return GetHeroGroupNumber(AVATAR_DAN_FARAWAY, AVATAR_DAN_NEARBY);
+	}
 	public static int GetYLKGroupNumber() {
 		return GetHeroGroupNumber(AVATAR_YLK_FARAWAY, AVATAR_YLK_NEARBY);
 	}
-	public static int GetDANGroupNumber() {
-		return GetHeroGroupNumber(AVATAR_DAN_FARAWAY, AVATAR_DAN_NEARBY);
+	public static int GetMRXGroupNumber() {
+		return GetHeroGroupNumber(AVATAR_MRX_FARAWAY, AVATAR_MRX_NEARBY);
 	}
 	public static readonly Vector2Int[] AVATAR_SAMPLE_POINTS = {
 		new Vector2Int(7, 7), new Vector2Int(15, 7), new Vector2Int(23, 7),
@@ -44,7 +49,7 @@ public static partial class Recognize {
 	public static readonly Color32[] AVATAR_YLK_FARAWAY = {
 		new Color32(118, 120, 129, 255), new Color32(147, 142, 142, 255), new Color32(186, 187, 188, 255),
 		new Color32(108, 103, 107, 255), new Color32(165, 138, 111, 255), new Color32(179, 166, 155, 255),
-												new Color32(89, 87, 85, 255),  
+												new Color32(89, 87, 85, 255),
 	};
 	public static readonly Color32[] AVATAR_YLK_NEARBY = {
 		new Color32(122, 121, 129, 255), new Color32(151, 148, 149, 255), new Color32(210, 212, 210, 255),
@@ -54,12 +59,22 @@ public static partial class Recognize {
 	public static readonly Color32[] AVATAR_DAN_FARAWAY = {
 		new Color32(240, 190, 95, 255), new Color32(73, 74, 124, 255), new Color32(57, 65, 107, 255),
 		new Color32(182, 127, 74, 255), new Color32(233, 168, 148, 255), new Color32(228, 160, 145, 255),
-												new Color32(232, 164, 133, 255), 
+												new Color32(232, 164, 133, 255),
 	};
 	public static readonly Color32[] AVATAR_DAN_NEARBY = {
 		new Color32(251, 193, 92, 255), new Color32(86, 80, 134, 255), new Color32(65, 57, 109, 255),
 		new Color32(172, 119, 76, 255), new Color32(231, 177, 156, 255), new Color32(222, 152, 134, 255),
-												new Color32(231, 134, 122, 255), 
+												new Color32(231, 134, 122, 255),
+	};
+	public static readonly Color32[] AVATAR_MRX_FARAWAY = {
+		new Color32(211, 132, 72, 255), new Color32(221, 141, 64, 255), new Color32(198, 116, 60, 255),
+		new Color32(147, 66, 70, 255), new Color32(186, 127, 90, 255), new Color32(231, 204, 203, 255),
+												new Color32(158, 105, 93, 255),
+	};
+	public static readonly Color32[] AVATAR_MRX_NEARBY = {
+		new Color32(211, 132, 72, 255), new Color32(221, 141, 64, 255), new Color32(198, 116, 60, 255),
+		new Color32(147, 66, 70, 255), new Color32(186, 127, 90, 255), new Color32(231, 204, 203, 255),
+												new Color32(158, 105, 93, 255),
 	};
 	public static int GetHeroGroupNumber(IReadOnlyList<Color32> heroAvatarFaraway, IReadOnlyList<Color32> heroAvatarNearby) {
 		int deltaY = -1;
@@ -95,16 +110,63 @@ public static partial class Recognize {
 					}
 					Color32 color = new Color32((byte) (r / 3), (byte) (g / 3), (byte) (b / 3), 255);
 					// Color32 color = ScreenshotUtils.GetColorOnScreen(finalPoint.x, finalPoint.y);
-					if (ApproximatelyCoveredCount(color, heroAvatar[i]) >= 0) {
+					if (ApproximatelyCoveredCount(color, heroAvatar[i], 1.4F) >= 0) {
 						++approximatelyCount;
 					}
 				}
-				if (approximatelyCount > pointCount * 0.75F) {
+				if (approximatelyCount > pointCount * 0.7F) {
 					return groupCount;
 				}
 				groupCount++;
 			}
 		}
 		return -1;
+	}
+	
+	[MenuItem("Assets/LogGroupHeroAvatar", priority = -1)]
+	public static void LogGroupHeroAvatar() {
+		int deltaY = IsOutsideNearby ? 76 : IsOutsideFaraway ? 0 : -1;
+		if (deltaY >= 0) {
+			int groupCount = 0;
+			Color32 targetColor1 = new Color32(98, 135, 229, 255);	// 无界面覆盖
+			Color32 targetColor2 = new Color32(29, 40, 68, 255);	// 联盟背包等窗口覆盖
+			Color32 targetColor3 = new Color32(9, 12, 20, 255);	// 曙光活动主界面（或双层窗口）覆盖
+			while (groupCount < 10) {
+				Color32 realColor = ScreenshotUtils.GetColorOnScreen(145, 438 + deltaY + groupCount * 50);
+				if (!Approximately(realColor, targetColor1, 10) &&
+						!Approximately(realColor, targetColor2, 10) &&
+						!Approximately(realColor, targetColor3, 10)) {
+					break;
+				}
+				Debug.LogError($"----------------------{groupCount}----------------------");
+				List<Color32> list = new List<Color32>();
+				for (int i = 0, length = AVATAR_SAMPLE_POINTS.Length; i < length; ++i) {
+					Vector2Int point = AVATAR_SAMPLE_POINTS[i];
+					Vector2Int finalPoint = new Vector2Int(22 + point.x, 418 + deltaY + groupCount * 50 + point.y);
+					int r = 0, g = 0, b = 0;
+					for (int y = -1; y < 2; ++y) {
+						Color32 c = ScreenshotUtils.GetColorOnScreen(finalPoint.x, finalPoint.y + y);
+						r += c.r;
+						g += c.g;
+						b += c.b;
+					}
+					Color32 color = new Color32((byte) (r / 3), (byte) (g / 3), (byte) (b / 3), 255);
+					list.Add(color);
+					// Debug.Log($"{finalPoint}: {color}");
+				}
+				StringBuilder sb = new StringBuilder();
+				foreach (var c in list) {
+					sb.Append("new Color32(");
+					sb.Append(c.r);
+					sb.Append(", ");
+					sb.Append(c.g);
+					sb.Append(", ");
+					sb.Append(c.b);
+					sb.Append(", 255), ");
+				}
+				Debug.LogError(sb);
+				groupCount++;
+			}
+		}
 	}
 }
