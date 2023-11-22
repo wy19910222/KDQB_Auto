b26e43597cdd70e851aad2b9793d3ea5f0d9322a
yun.wang <wy19910222@hotmail.com>
2023-11-23 04:00:24 +0800

    忙碌队列数量判断bug修复

--- Assets/Editor/Recognize_Hero.cs
+++ Assets/Editor/Recognize_Hero.cs
@@ -67,8 +67,8 @@ public static partial class Recognize {
 		}
 		if (deltaY >= 0 && heroAvatar != null) {
 			int groupCount = 0;
-			// 返回加速等蓝色按钮
-			Color32 targetColor = new Color32(98, 135, 229, 255);
+			// 返回加速等蓝色按钮中间的白色
+			Color32 targetColor = new Color32(255, 255, 255, 255);
 			Color32[,] realColors = ScreenshotUtils.GetColorsOnScreen(0, 400 + deltaY, 160, 500);
 			while (groupCount < 10) {
 				Color32 realColor = realColors[158, 34 + groupCount * 50];
