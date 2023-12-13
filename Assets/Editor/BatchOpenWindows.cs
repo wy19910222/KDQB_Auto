c19e2905c32421c0e80dfa65e8143f4f19d91af4
yun.wang <wy19910222@hotmail.com>
2023-12-13 18:49:04 +0800

    批量打开窗口

---
+++ Assets/Editor/BatchOpenWindows.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+public static class BatchOpenWindows {
+	[MenuItem("Window/Default/OpenAll")]
+	private static void OpenDefaultWindows() {
+		EditorApplication.ExecuteMenuItem("Window/Default/Follow");
+		EditorApplication.ExecuteMenuItem("Window/Default/Jungle");
+		EditorApplication.ExecuteMenuItem("Window/Default/Gather");
+		EditorApplication.ExecuteMenuItem("Window/Default/AttackMarshal");
+		EditorApplication.ExecuteMenuItem("Window/Default/FreeDiamond");
+		EditorApplication.ExecuteMenuItem("Window/Default/AllianceMechaDonate");
+		EditorApplication.ExecuteMenuItem("Window/Default/ConnectingMonitoring");
+		EditorApplication.ExecuteMenuItem("Window/Default/BlockBreak");
+		EditorApplication.ExecuteMenuItem("Window/Default/AutoFix");
+		EditorApplication.ExecuteMenuItem("Window/Default/ExtensionalScreen");
+	}
+}
