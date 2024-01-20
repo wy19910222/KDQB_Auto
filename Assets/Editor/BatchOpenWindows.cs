683462168b69568b6bcbd09b948b7bdba824b3db
456 <wy1991022219910222@163.com>
2024-01-20 19:14:20 +0800

    多打开GlobalStatus

--- Assets/Editor/BatchOpenWindows.cs
+++ Assets/Editor/BatchOpenWindows.cs
@@ -3,16 +3,17 @@
 public static class BatchOpenWindows {
 	[MenuItem("Tools_Window/Default/OpenAll")]
 	private static void OpenDefaultWindows() {
+		EditorApplication.ExecuteMenuItem("Tools_Window/Default/GlobalStatus");
 		EditorApplication.ExecuteMenuItem("Tools_Window/Default/Follow");
 		EditorApplication.ExecuteMenuItem("Tools_Window/Default/Jungle");
 		EditorApplication.ExecuteMenuItem("Tools_Window/Default/Gather");
 		EditorApplication.ExecuteMenuItem("Tools_Window/Default/AttackMarshal");
 		EditorApplication.ExecuteMenuItem("Tools_Window/Default/FreeDiamond");
 		EditorApplication.ExecuteMenuItem("Tools_Window/Default/AllianceMechaDonate");
+		EditorApplication.ExecuteMenuItem("Tools_Window/Default/AutoFix");
 		EditorApplication.ExecuteMenuItem("Tools_Window/Default/AllianceHelp");
+		EditorApplication.ExecuteMenuItem("Tools_Window/Default/ExtensionalScreen");
 		EditorApplication.ExecuteMenuItem("Tools_Window/Default/ConnectingMonitoring");
 		EditorApplication.ExecuteMenuItem("Tools_Window/Default/BlockBreak");
-		EditorApplication.ExecuteMenuItem("Tools_Window/Default/AutoFix");
-		EditorApplication.ExecuteMenuItem("Tools_Window/Default/ExtensionalScreen");
 	}
 }
