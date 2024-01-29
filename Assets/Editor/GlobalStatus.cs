89a9c2dade352e02dea158dfce33001d5c9f836f
456 <wy1991022219910222@163.com>
2024-01-29 18:57:40 +0800

    新增无人值守判断

--- Assets/Editor/GlobalStatus.cs
+++ Assets/Editor/GlobalStatus.cs
@@ -5,6 +5,8 @@
  * @EditTime: 2023-12-28 12:59:39 971
  */
 
+using System;
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 
@@ -23,6 +25,7 @@ public class GlobalStatusConfig : EditorWindow {
 
 	private void OnGUI() {
 		EditorGUILayout.RectField("游戏范围", Operation.CURRENT_GAME_RECT);
+		EditorGUILayout.Toggle("无人值守", GlobalStatus.IsUnattended);
 		EditorGUILayout.BeginHorizontal();
 		EditorGUILayout.LabelField("当前任务", GUILayout.Width(EditorGUIUtility.labelWidth));
 		EditorGUILayout.LabelField(Task.CurrentTask ?? "Idle", GUILayout.MinWidth(0));
@@ -39,9 +42,52 @@ public class GlobalStatusConfig : EditorWindow {
 		Recognize.ENERGY_FULL = EditorGUILayout.IntField(Recognize.ENERGY_FULL);
 		EditorGUILayout.EndHorizontal();
 		EditorGUILayout.FloatField("窗口覆盖", Recognize.WindowCoveredCount);
+		// EditorGUILayout.IntField("戴安娜所在队列", Recognize.GetHeroGroupNumber(Recognize.HeroType.DAN));
+		// EditorGUILayout.IntField("尤里卡所在队列", Recognize.GetHeroGroupNumber(Recognize.HeroType.YLK));
+		// EditorGUILayout.IntField("明日香所在队列", Recognize.GetHeroGroupNumber(Recognize.HeroType.MRX));
 	}
 	
 	private void Update() {
 		Repaint();
+	}
+}
+
+public static class GlobalStatus {
+	public static bool IsUnattended { get; private set; }
+	public const long UNATTENDED_THRESHOLD = 30 * 1000 * 1000 * 10; // 30秒
+
+	private static EditorCoroutine s_CO;
+	
+
+	[InitializeOnLoadMethod]
+	private static void Enable() {
+		Disable();
+		s_CO = EditorCoroutineManager.StartCoroutine(Update());
+		Debug.Log($"全局状态监测已开启");
+	}
+
+	private static void Disable() {
+		if (s_CO != null) {
+			EditorCoroutineManager.StopCoroutine(s_CO);
+			s_CO = null;
+			Debug.Log("全局状态监测已关闭");
+		}
+	}
+
+	private static IEnumerator Update() {
+		Vector2Int prevMousePos = MouseUtils.GetMousePos();
+		DateTime startDT = DateTime.Now;
+		while (true) {
+			yield return null;
+			Vector2Int nextMousePos = MouseUtils.GetMousePos();
+			DateTime now = DateTime.Now;
+			if (nextMousePos.x != prevMousePos.x || nextMousePos.y != prevMousePos.y) {
+				prevMousePos = nextMousePos;
+				startDT = now;
+				IsUnattended = false;
+			} else if ((now - startDT).Ticks > UNATTENDED_THRESHOLD) {
+				IsUnattended = true;
+			}
+		}
 	}
 }
