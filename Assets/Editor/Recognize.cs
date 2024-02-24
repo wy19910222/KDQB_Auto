11d9579f0bf3fccdb364b61e0c3688b215a1488a
yun.wang <wy1991022219910222@163.com>
2024-02-25 04:30:50 +0800

    对免费钻石相关的判断，兼容列表5像素内的偏移

--- Assets/Editor/Recognize.cs
+++ Assets/Editor/Recognize.cs
@@ -146,16 +146,44 @@ public static partial class Recognize {
 	private static readonly Color32[,] FREE_DIAMOND_COOL_DOWN = Operation.GetFromFile("PersistentData/Textures/FreeDiamondCoolDown.png");
 	public static bool IsFreeDiamondCoolDown {
 		get {
-			Color32[,] realColors = Operation.GetColorsOnScreen(1099, 295, 55, 30);
-			return ApproximatelyRect(realColors, FREE_DIAMOND_COOL_DOWN) > 0.9F;
+			const int WIDTH = 55, HEIGHT = 30;
+			const int OFFSET_MAX = -5;
+			Color32[,] realColors = Operation.GetColorsOnScreen(1099, 295 + Mathf.Min(OFFSET_MAX, 0), WIDTH, HEIGHT + Mathf.Abs(OFFSET_MAX));
+			int sign = Mathf.RoundToInt(Mathf.Sign(OFFSET_MAX));
+			for (int i = 0; i < Mathf.Abs(OFFSET_MAX); i++) {
+				Color32[,] _realColors = new Color32[WIDTH, HEIGHT];
+				for (int x = 0; x < WIDTH; x++) {
+					for (int y = 0; y < HEIGHT; y++) {
+						_realColors[x, y] = realColors[x, y - Mathf.Min(OFFSET_MAX, 0) + i * sign];
+					}
+				}
+				if (ApproximatelyRect(_realColors, FREE_DIAMOND_COOL_DOWN) > 0.9F) {
+					return true;
+				}
+			}
+			return false;
 		}
 	}
 
 	private static readonly Color32[,] FREE_DIAMOND_NO_COUNTDOWN = Operation.GetFromFile("PersistentData/Textures/FreeDiamondNoCountdown.png");
 	public static bool IsFreeDiamondNoCountdown {
 		get {
-			Color32[,] realColors = Operation.GetColorsOnScreen(1096, 261, 58, 15);
-			return ApproximatelyRect(realColors, FREE_DIAMOND_NO_COUNTDOWN) > 0.9F;
+			const int WIDTH = 58, HEIGHT = 15;
+			const int OFFSET_MAX = -5;
+			Color32[,] realColors = Operation.GetColorsOnScreen(1096, 261 + Mathf.Min(OFFSET_MAX, 0), WIDTH, HEIGHT + Mathf.Abs(OFFSET_MAX));
+			int sign = Mathf.RoundToInt(Mathf.Sign(OFFSET_MAX));
+			for (int i = 0; i < Mathf.Abs(OFFSET_MAX); i++) {
+				Color32[,] _realColors = new Color32[WIDTH, HEIGHT];
+				for (int x = 0; x < WIDTH; x++) {
+					for (int y = 0; y < HEIGHT; y++) {
+						_realColors[x, y] = realColors[x, y - Mathf.Min(OFFSET_MAX, 0) + i * sign];
+					}
+				}
+				if (ApproximatelyRect(_realColors, FREE_DIAMOND_NO_COUNTDOWN) > 0.9F) {
+					return true;
+				}
+			}
+			return false;
 		}
 	}
 	
