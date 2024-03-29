27f7a07497ef72d3808a7dcc8d8ac2d8b6980bd4
yun.wang <wy1991022219910222@163.com>
2024-03-29 23:23:55 +0800

    网络状态监测

---
+++ Assets/Editor/Scripts/Utils/Network.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+public static class Network {
+	public static bool IsInternetAvailable() {
+		try {
+			Dns.GetHostEntry("www.baidu.com");
+			return true;
+		} catch {
+			return false;
+		}
+	}
+}
