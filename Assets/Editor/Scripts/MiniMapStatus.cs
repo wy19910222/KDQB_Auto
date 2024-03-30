fe7c735cb1740cecbc6c0cbcf4a586d3b0c9c1bf
yun.wang <wy1991022219910222@163.com>
2024-03-30 18:30:48 +0800

    小地图状态监管

---
+++ Assets/Editor/Scripts/MiniMapStatus.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2024-03-30 18:08:32 761
+ * @LastEditor: wangyun
+ * @EditTime: 2024-03-30 18:08:32 765
+ */
+
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class MiniMapStatus {
+	public static bool KEEP_SHOWING = false;	// 监测间隔
+	
+	private static EditorCoroutine s_CO;
+	public static bool IsRunning => s_CO != null;
+
+	[MenuItem("Tools_Task/StartMiniMapStatus", priority = -1)]
+	private static void Enable() {
+		Disable();
+		Debug.Log($"自动修理已开启");
+		s_CO = EditorCoroutineManager.StartCoroutine(Update());
+	}
+
+	[MenuItem("Tools_Task/StopMiniMapStatus", priority = -1)]
+	private static void Disable() {
+		if (s_CO != null) {
+			EditorCoroutineManager.StopCoroutine(s_CO);
+			s_CO = null;
+			Debug.Log("自动修理已关闭");
+		}
+	}
+
+	private static IEnumerator Update() {
+		while (true) {
+			yield return null;
+			
+			bool? isMiniMapShowing = Recognize.IsMiniMapShowing;
+			if (isMiniMapShowing != null && isMiniMapShowing != KEEP_SHOWING) {
+				if (Task.CurrentTask != null) {
+					continue;
+				}
+				Task.CurrentTask = nameof(MiniMapStatus);
+				
+				Debug.Log("小地图展开收起按钮");
+				Operation.Click(27, 237);	// 修理按钮按钮
+				
+				Task.CurrentTask = null;
+			}
+		}
+		// ReSharper disable once IteratorNeverReturns
+	}
+}
---
+++ Assets/Editor/Window/MiniMapStatusConfig.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2024-03-30 18:08:32 761
+ * @LastEditor: wangyun
+ * @EditTime: 2024-03-30 18:08:32 765
+ */
+
+using UnityEngine;
+using UnityEditor;
+
+public class MiniMapStatusConfig : PrefsEditorWindow<MiniMapStatus> {
+	[MenuItem("Tools_Window/Default/MiniMapStatus", false, -1)]
+	private static void Open() {
+		GetWindow<MiniMapStatusConfig>("小地图状态").Show();
+	}
+	
+	private void OnGUI() {
+		MiniMapStatus.KEEP_SHOWING = GUILayout.Toggle(MiniMapStatus.KEEP_SHOWING, "保持展开状态", "Button");
+		GUILayout.Space(5F);
+		if (MiniMapStatus.IsRunning) {
+			if (GUILayout.Button("关闭")) {
+				IsRunning = false;
+			}
+		} else {
+			if (GUILayout.Button("开启")) {
+				IsRunning = true;
+			}
+		}
+	}
+}
