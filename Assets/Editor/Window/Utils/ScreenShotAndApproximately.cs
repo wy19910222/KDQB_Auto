a61f1b7f327acc767400e38d39d263074b695e01
yun.wang <wy1991022219910222@163.com>
2024-04-09 15:54:04 +0800

    截取与对比工具

---
+++ Assets/Editor/Window/Utils/ScreenShotAndApproximately.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2023-11-05 19:03:52 124
+ * @LastEditor: wangyun
+ * @EditTime: 2023-11-05 19:03:52 128
+ */
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ScreenShotAndApproximately : EditorWindow {
+	[MenuItem("Tools_Window/ScreenShotAndApproximately", false, 22)]
+	private static void Open() {
+		GetWindow<ScreenShotAndApproximately>("截取和对比").Show();
+	}
+
+	[SerializeField]
+	private RectInt rect = new RectInt();
+	[SerializeField]
+	private string filename = string.Empty;
+
+	private void OnGUI() {
+		EditorGUI.BeginChangeCheck();
+		RectInt newRect = EditorGUILayout.RectIntField(rect);
+		if (EditorGUI.EndChangeCheck()) {
+			Undo.RecordObject(this, "Rect");
+			rect = newRect;
+		}
+		string newFilename = EditorGUILayout.TextField(filename);
+		if (newFilename != filename) {
+			Undo.RecordObject(this, "Filename");
+			filename = newFilename;
+		}
+		EditorGUILayout.BeginHorizontal();
+		if (GUILayout.Button("截取")) {
+			if (!string.IsNullOrEmpty(filename)) {
+				Operation.Screenshot(rect.x, rect.y, rect.width, rect.height, $"{Application.dataPath}/PersistentData/Textures/{filename}.png");
+			} else {
+				Debug.LogError("Filename is empty!");
+			}
+		}
+		if (GUILayout.Button("对比")) {
+			Color32[,] targetColors = Operation.GetFromFile("PersistentData/Textures/DailyIntelligenceBtn.png");
+			Color32[,] realColors = Operation.GetColorsOnScreen(718, 852, 31, 31);
+			Debug.Log(Recognize.ApproximatelyRect(realColors, targetColors));
+		}
+		EditorGUILayout.EndHorizontal();
+	}
+}
