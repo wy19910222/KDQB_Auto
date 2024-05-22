ce8883e11355b24d4631a0b98674b45dc0f9e12a
yun.wang <wy1991022219910222@163.com>
2024-05-23 00:13:00 +0800

    联盟帮助奖励图标判断阈值修改

--- Assets/Editor/Scripts/Recognize/Recognize_Alliance.cs
+++ Assets/Editor/Scripts/Recognize/Recognize_Alliance.cs
@@ -170,7 +170,7 @@ public static partial class Recognize {
 		get {
 			return GetCachedValueOrNew(nameof(IsAllianceHelpAwardOuter), () => {
 				Color32[,] realColors = Operation.GetColorsOnScreen(1888, 680, 19, 16);
-				return ApproximatelyRect(realColors, ALLIANCE_HELP_AWARD_OUTER, 2F) > 0.8F;
+				return ApproximatelyRect(realColors, ALLIANCE_HELP_AWARD_OUTER, 2F) > 0.7F;
 			});
 		}
 	}
