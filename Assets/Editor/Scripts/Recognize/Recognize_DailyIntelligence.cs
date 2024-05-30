34589a717adfa229c443c543dc3c4b52c4c0d702
yun.wang <wy1991022219910222@163.com>
2024-05-30 21:33:06 +0800

    新增每日军情判断的类别

--- Assets/Editor/Scripts/Recognize/Recognize_DailyIntelligence.cs
+++ Assets/Editor/Scripts/Recognize/Recognize_DailyIntelligence.cs
@@ -15,6 +15,10 @@ public static partial class Recognize {
 		WILD,
 		[InspectorName("远征行动")]
 		EXPEDITION,
+		[InspectorName("沙盘演习")]
+		SAND_TABLE,
+		[InspectorName("岛屿作战")]
+		ISLAND,
 	}
 	
 	private static readonly Color32[,] DAILY_INTELLIGENCE_BTN = Operation.GetFromFile("PersistentData/Textures/DailyIntelligenceBtn.png");
@@ -27,6 +31,7 @@ public static partial class Recognize {
 
 	private static readonly Color32[,] DAILY_INTELLIGENCE_TITLE_WILD = Operation.GetFromFile("PersistentData/Textures/DailyIntelligenceTitleWild.png");
 	private static readonly Color32[,] DAILY_INTELLIGENCE_TITLE_EXPEDITION = Operation.GetFromFile("PersistentData/Textures/DailyIntelligenceTitleExpedition.png");
+	private static readonly Color32[,] DAILY_INTELLIGENCE_TITLE_SAND_TABLE = Operation.GetFromFile("PersistentData/Textures/DailyIntelligenceTitleSandTable.png");
 	public static DailyIntelligenceType DailyIntelligenceCurrentType {
 		get {
 			Color32[,] realColors = Operation.GetColorsOnScreen(896, 112, 131, 30);
@@ -36,10 +41,21 @@ public static partial class Recognize {
 			else if (ApproximatelyRectIgnoreCovered(realColors, DAILY_INTELLIGENCE_TITLE_EXPEDITION) > 0.9F) {
 				return DailyIntelligenceType.EXPEDITION;
 			}
-			return DailyIntelligenceType.UNKNOWN;
+			else if (ApproximatelyRectIgnoreCovered(realColors, DAILY_INTELLIGENCE_TITLE_SAND_TABLE) > 0.9F) {
+				return DailyIntelligenceType.SAND_TABLE;
+			}
+			return IsIsland ? DailyIntelligenceType.ISLAND : DailyIntelligenceType.UNKNOWN;
 		}
 	}
-
+	
+	private static readonly Color32[,] DAILY_INTELLIGENCE_ISLAND_RESET_TIMES_LABEL = Operation.GetFromFile("PersistentData/Textures/DailyIntelligenceIslandResetTimesLabel.png");
+	private static bool IsIsland {
+		get {
+			Color32[,] realColors = Operation.GetColorsOnScreen(714, 983, 80, 20);
+			return ApproximatelyRect(realColors, DAILY_INTELLIGENCE_ISLAND_RESET_TIMES_LABEL) > 0.7F;
+		}
+	}
+	
 	private static readonly Color32[,] DAILY_INTELLIGENCE_GET_BTN = Operation.GetFromFile("PersistentData/Textures/DailyIntelligenceGetBtn.png");
 	private static readonly Color32[,] DAILY_INTELLIGENCE_BACK_BTN = Operation.GetFromFile("PersistentData/Textures/DailyIntelligenceBackBtn.png");
 	public static bool IsWildGetBtn {
