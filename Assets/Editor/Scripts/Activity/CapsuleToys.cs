e379f2c38735243c06e9234052a324a27b2020a1
yun.wang <wy1991022219910222@163.com>
2024-06-01 16:12:02 +0800

    自动扭蛋脚本

---
+++ Assets/Editor/Scripts/Activity/CapsuleToys.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2024-06-01 15:12:53 886
+ * @LastEditor: wangyun
+ * @EditTime: 2024-06-01 15:12:53 892
+ */
+
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class CapsuleToys {
+	public static KeyboardUtils.VKCode KEY_START = KeyboardUtils.VKCode.F1;
+	public static KeyboardUtils.VKCode KEY_STOP = KeyboardUtils.VKCode.F2;
+	public static  int LUCK_LEVEL;
+	public static readonly bool[][] GET_OF_STAR_PLANS = {
+		new[] { true,	true,	true,	true,	true,	true,	true },	// 自定义，可修改
+		new[] { true,	true,	true,	true,	true,	false,	true },	// 非酋
+		new[] { true,	false,	false,	false,	false,	false,	true },	// 黑人
+		new[] { true,	false,	false,	false,	false,	true,	true },	// 普通
+		new[] { false,	false,	false,	false,	false,	true,	true },	// 白人
+		new[] { false,	false,	false,	false,	true,	true,	true },	// 欧皇
+	};
+	
+	public static bool IsRunning;
+
+	[MenuItem("Tools_Task/StartCapsuleToys", priority = -1)]
+	private static void Enable() {
+		Disable();
+		Debug.Log($"扭蛋快捷键已开启");
+		IsRunning = true;
+		KeyboardUtils.OnKeyUp += OnKeyUp;
+		KeyboardUtils.Hook();
+	}
+
+	[MenuItem("Tools_Task/StopCapsuleToys", priority = -1)]
+	private static void Disable() {
+		if (IsRunning) {
+			KeyboardUtils.Unhook();
+			KeyboardUtils.OnKeyUp -= OnKeyUp;
+			IsRunning = false;
+			Debug.Log("扭蛋快捷键已关闭");
+		}
+	}
+
+	private static void OnKeyUp(int vkCode) {
+		KeyboardUtils.VKCode key = (KeyboardUtils.VKCode) vkCode;
+		if (key == KEY_START) {
+			Debug.Log($"开始自动扭蛋");
+			StartCapsuleToys();
+		} else if (key == KEY_STOP) {
+			Debug.Log($"终止自动扭蛋");
+			StopCapsuleToys();
+		}
+	}
+
+	private static EditorCoroutine s_Co;
+	private static void StopCapsuleToys() {
+		if (s_Co != null) {
+			EditorCoroutineManager.StopCoroutine(s_Co);
+		}
+	}
+
+	private static void StartCapsuleToys() {
+		StopCapsuleToys();
+		s_Co = EditorCoroutineManager.StartCoroutine(IECapsuleToys());
+	}
+	
+	private static IEnumerator IECapsuleToys() {
+		while (true) {
+			Operation.Click(900, 970);	// 开始扭蛋
+			yield return new EditorWaitForSeconds(2.5F);
+			int star = Recognize.CurrentCapsuleToyStar;
+			bool validStar = star is > 0 and <= 7;
+			bool willGet = !validStar || GET_OF_STAR_PLANS[LUCK_LEVEL][star - 1];
+			if (willGet) {
+				Operation.Click(990, 860);	// 领取奖励
+				yield return new EditorWaitForSeconds(0.3F);
+				Operation.Click(1060, 730);	// 领取奖励
+				yield return new EditorWaitForSeconds(0.5F);
+				Operation.Click(960, 970);	// 点击继续
+				yield return new EditorWaitForSeconds(0.2F);
+			}
+		}
+		// ReSharper disable once IteratorNeverReturns
+	}
+}
---
+++ Assets/Editor/Window/CapsuleToysConfig.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2024-06-01 15:12:53 886
+ * @LastEditor: wangyun
+ * @EditTime: 2024-06-01 15:12:53 892
+ */
+
+using System;
+using UnityEngine;
+using UnityEditor;
+
+public class CapsuleToysConfig : PrefsEditorWindow<CapsuleToys> {
+	[MenuItem("Tools_Window/Activity/CapsuleToys")]
+	private static void Open() {
+		GetWindow<CapsuleToysConfig>("自动扭蛋").Show();
+	}
+
+	private static readonly string[] s_LuckLevelNames = {"自定义", "非酋", "黑人", "普通", "白人", "欧皇"};
+
+	private void OnGUI() {
+		CapsuleToys.KEY_START = (KeyboardUtils.VKCode) EditorGUILayout.EnumPopup("开始快捷键", CapsuleToys.KEY_START);
+		CapsuleToys.KEY_STOP = (KeyboardUtils.VKCode) EditorGUILayout.EnumPopup("终止快捷键", CapsuleToys.KEY_STOP);
+		EditorGUILayout.LabelField("方案");
+		EditorGUILayout.BeginHorizontal();
+		for (int i = 0, length = CapsuleToys.GET_OF_STAR_PLANS.Length; i < length; i++) {
+			bool selected = i == CapsuleToys.LUCK_LEVEL;
+			bool newSelected = GUILayout.Toggle(i == CapsuleToys.LUCK_LEVEL, s_LuckLevelNames[i], "Button");
+			if (newSelected && !selected) {
+				CapsuleToys.LUCK_LEVEL = i;
+			}
+		}
+		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.LabelField("收取星级");
+		EditorGUI.BeginDisabledGroup(CapsuleToys.LUCK_LEVEL != 0);
+		EditorGUILayout.BeginHorizontal();
+		bool[] getOfStar = CapsuleToys.GET_OF_STAR_PLANS[CapsuleToys.LUCK_LEVEL];
+		for (int i = 0, length = getOfStar.Length; i < length; i++) {
+			getOfStar[i] = GUILayout.Toggle(getOfStar[i], $"{i + 1}星", "Button");
+		}
+		EditorGUILayout.EndHorizontal();
+		EditorGUI.EndDisabledGroup();
+		
+		GUILayout.Space(5F);
+		if (CapsuleToys.IsRunning) {
+			if (GUILayout.Button("关闭")) {
+				IsRunning = false;
+			}
+		} else {
+			if (GUILayout.Button("开启")) {
+				IsRunning = true;
+			}
+		}
+	}
+}
